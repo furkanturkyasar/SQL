173f62bfe5389efe004377cfbc2e4ba8d5afb510
Furkan Türkyaşar <53195046+furkanturkyasar@users.noreply.github.com>
2022-02-04 17:11:53 +0300

    Create hw7.sql

---
+++ Ödev7/hw7.sql
@@ -0,0 +1,13 @@
+SELECT rating FROM film
+GROUP BY rating;
+
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING (COUNT(*)) > 50;
+
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+SELECT country_id, COUNT(city) FROM city
+ORDER BY COUNT(city) DESC
+LIMIT 1;
